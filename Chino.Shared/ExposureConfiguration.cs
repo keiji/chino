2851a6b70473b2538767a915d0279730b22a0b91
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-04-20 18:00:49 +0900

    Documented

--- Chino.Shared/ExposureConfiguration.cs
+++ Chino.Shared/ExposureConfiguration.cs
@@ -13,22 +13,56 @@ namespace Chino
         // https://developers.google.com/android/reference/com/google/android/gms/nearby/exposurenotification/ExposureConfiguration
         public class GoogleExposureConfiguration
         {
+
+            /*
+             * [0] when Attenuation > 73 dB
+             * [1] when 73 >= Attenuation > 63
+             * [2] when 63 >= Attenuation > 51
+             * [3] when 51 >= Attenuation > 33
+             * [4] when 33 >= Attenuation > 27
+             * [5] when 27 >= Attenuation > 15
+             * [6] when 15 >= Attenuation > 10
+             * [7] when 10 >= Attenuation
+             */
             public int[] AttenuationScores { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
             public int AttenuationWeight { get; set; } = 50;
 
+            /*
+             * [0] when Days >= 14
+             * [1] when Days >= 12
+             * [2] when Days >= 10
+             * [3] when Days >= 8
+             * [4] when Days >= 6
+             * [5] when Days >= 4
+             * [6] when Days >= 2
+             * [7] when Days >= 0
+             */
             public int[] DaysSinceLastExposureScores { get; set; } = { 1, 1, 1, 1, 1, 1, 1, 1 };
 
             public int DaysSinceLastExposureWeight { get; set; } = 50;
 
             public int[] DurationAtAttenuationThresholds { get; set; } = { 50, 70 };
 
+            /*
+             * [0] when Duration == 0 min
+             * [1] when Duration <= 5
+             * [2] when Duration <= 10
+             * [3] when Duration <= 15
+             * [4] when Duration <= 20
+             * [5] when Duration <= 25
+             * [6] when Duration <= 30
+             * [7] when Duration > 30
+             */
             public int[] DurationScores { get; set; } = { 1, 1, 1, 1, 1, 1, 1, 1 };
 
             public int DurationWeight { get; set; } = 50;
 
             public int MinimumRiskScore { get; set; } = 21;
 
+            /*
+             * [0]-[7] App Defined
+             */
             public int[] TransmissionRiskScores { get; set; } = { 7, 7, 7, 7, 7, 7, 7, 7 };
 
             public int TransmissionRiskWeight { get; set; } = 50;
@@ -134,12 +168,45 @@ namespace Chino
             public double ReportTypeSelfReportedWeight { get; set; } = 100.0;
             public ReportType ReportTypeNoneMap { get; set; } = ReportType.Unknown;
 
+            /*
+             * [0] when Attenuation > 73 dB
+             * [1] when 73 >= Attenuation > 63
+             * [2] when 63 >= Attenuation > 51
+             * [3] when 51 >= Attenuation > 33
+             * [4] when 33 >= Attenuation > 27
+             * [5] when 27 >= Attenuation > 15
+             * [6] when 15 >= Attenuation > 10
+             * [7] when 10 >= Attenuation
+             */
             public int[] AttenuationLevelValues { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
+            /*
+             * [0] when Days >= 14
+             * [1] when Days >= 12
+             * [2] when Days >= 10
+             * [3] when Days >= 8
+             * [4] when Days >= 6
+             * [5] when Days >= 4
+             * [6] when Days >= 2
+             * [7] when Days >= 0
+             */
             public int[] DaysSinceLastExposureLevelValues { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
+            /*
+             * [0] when Duration == 0 min
+             * [1] when Duration <= 5
+             * [2] when Duration <= 10
+             * [3] when Duration <= 15
+             * [4] when Duration <= 20
+             * [5] when Duration <= 25
+             * [6] when Duration <= 30
+             * [7] when Duration > 30
+             */
             public int[] DurationLevelValues { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
+            /*
+             * [0]-[7] App Defined
+             */
             public int[] TransmissionRiskLevelValues { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
             public IDictionary<object, object> Metadata { get; set; } = new Dictionary<object, object>();
