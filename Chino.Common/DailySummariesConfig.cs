8d665675e3c18e1a3df80cd5bdfc63789023d92c
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-04-27 15:52:21 +0900

    Add JSON.NET package

--- Chino.Common/DailySummariesConfig.cs
+++ Chino.Common/DailySummariesConfig.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Chino
 {
     // https://developers.google.com/android/reference/com/google/android/gms/nearby/exposurenotification/DailySummariesConfig
+    [JsonObject]
     public class DailySummariesConfig
     {
-        // Each element must be between 0 and 255
+        [JsonProperty("attenuation_bucket_threshold_db")]
         public IList<int> AttenuationBucketThresholdDb { get; set; } = new int[] { 50, 70, 90 };
 
         // Each element must be between 0 and 2.5
+        [JsonProperty("attenuation_bucket_weights")]
         public IList<double> AttenuationBucketWeights { get; set; } = new double[] { 1.0, 1.0, 1.0, 1.0 };
 
+        [JsonProperty("days_since_exposure_threshold")]
         public int DaysSinceExposureThreshold { get; set; } = 0;
 
         // Each element must be between 0 and 2.5
+        [JsonProperty("infectiousness_weights")]
         public IDictionary<Infectiousness, double> InfectiousnessWeights { get; set; } = new Dictionary<Infectiousness, double>()
         {
             { Infectiousness.High, 1.0 },
@@ -21,9 +26,11 @@ namespace Chino
             { Infectiousness.None, 1.0 },
         };
 
-        public double MinimumWindowScore { get; set; } = 0;
+        [JsonProperty("minimum_risk_score")]
+        public double MinimumWindowScore { get; set; } = 0.0;
 
         // Each element must be between 0 and 2.5
+        [JsonProperty("report_type_weights")]
         public IDictionary<ReportType, double> ReportTypeWeights { get; set; } = new Dictionary<ReportType, double>()
         {
             { ReportType.ConfirmedTest, 1.0 },
--- Chino.Common/ExposureConfiguration.cs
+++ Chino.Common/ExposureConfiguration.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Chino
 {
+    [JsonObject]
     public class ExposureConfiguration
     {
+        [JsonProperty("google_exposure_config")]
         public GoogleExposureConfiguration GoogleExposureConfig { get; set; } = new GoogleExposureConfiguration();
 
         // for ExposureWindow Mode
+        [JsonProperty("google_diagnosis_keys_data_mapping_config")]
         public GoogleDiagnosisKeysDataMappingConfiguration GoogleDiagnosisKeysDataMappingConfig { get; set; } = new GoogleDiagnosisKeysDataMappingConfiguration();
+
+        [JsonProperty("google_daily_summaries_config")]
         public DailySummariesConfig GoogleDailySummariesConfig { get; set; } = new DailySummariesConfig();
 
+        [JsonProperty("apple_exposure_v1_config")]
         public AppleExposureV1Configuration AppleExposureV1Config { get; set; } = new AppleExposureV1Configuration();
 
+        [JsonProperty("apple_exposure_v2_config")]
         public AppleExposureV2Configuration AppleExposureV2Config { get; set; } = new AppleExposureV2Configuration();
 
         // https://developers.google.com/android/reference/com/google/android/gms/nearby/exposurenotification/ExposureConfiguration
+        [JsonObject]
         public class GoogleExposureConfiguration
         {
 
@@ -28,8 +37,10 @@ namespace Chino
              * [6] when 15 >= Attenuation > 10
              * [7] when 10 >= Attenuation
              */
+            [JsonProperty("attenuation_scores")]
             public int[] AttenuationScores { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
+            [JsonProperty("attenuation_weight")]
             public int AttenuationWeight { get; set; } = 50;
 
             /*
@@ -42,10 +53,13 @@ namespace Chino
              * [6] when Days >= 2
              * [7] when Days >= 0
              */
+            [JsonProperty("days_since_last_exposure_scores")]
             public int[] DaysSinceLastExposureScores { get; set; } = { 1, 1, 1, 1, 1, 1, 1, 1 };
 
+            [JsonProperty("days_since_last_exposure_weight")]
             public int DaysSinceLastExposureWeight { get; set; } = 50;
 
+            [JsonProperty("duration_at_attenuation_thresholds")]
             public int[] DurationAtAttenuationThresholds { get; set; } = { 50, 70 };
 
             /*
@@ -58,23 +72,30 @@ namespace Chino
              * [6] when Duration <= 30
              * [7] when Duration > 30
              */
+            [JsonProperty("duration_scores")]
             public int[] DurationScores { get; set; } = { 1, 1, 1, 1, 1, 1, 1, 1 };
 
+            [JsonProperty("duration_weight")]
             public int DurationWeight { get; set; } = 50;
 
+            [JsonProperty("minimum_risk_score")]
             public int MinimumRiskScore { get; set; } = 21;
 
             /*
              * [0]-[7] App Defined
              */
+            [JsonProperty("transmission_risk_scores")]
             public int[] TransmissionRiskScores { get; set; } = { 7, 7, 7, 7, 7, 7, 7, 7 };
 
+            [JsonProperty("transmission_risk_weight")]
             public int TransmissionRiskWeight { get; set; } = 50;
         }
 
         // https://developers.google.com/android/exposure-notifications/meaningful-exposures
+        [JsonObject]
         public class GoogleDiagnosisKeysDataMappingConfiguration
         {
+            [JsonProperty("infectiousness_for_days_since_onset_of_symptoms")]
             public IDictionary<int, Infectiousness> InfectiousnessForDaysSinceOnsetOfSymptoms { get; set; } = new Dictionary<int, Infectiousness>() {
                 { -14, Infectiousness.High },
                 { -13, Infectiousness.High },
@@ -107,10 +128,12 @@ namespace Chino
                 { 14, Infectiousness.High },
             };
 
+            [JsonProperty("infectiousness_when_days_since_onset_missing")]
             public Infectiousness InfectiousnessWhenDaysSinceOnsetMissing = Infectiousness.Standard;
         }
 
         // https://developer.apple.com/documentation/exposurenotification/enexposureconfiguration/exposure_risk_value_calculation_in_exposurenotification_version_1
+        [JsonObject]
         public class AppleExposureV1Configuration
         {
             // Configuring Duration
@@ -127,6 +150,7 @@ namespace Chino
              * [6] when 15 >= Attenuation > 10
              * [7] when 10 >= Attenuation
              */
+            [JsonProperty("attenuation_level_values")]
             public int[] AttenuationLevelValues { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
             /*
@@ -139,6 +163,7 @@ namespace Chino
              * [6] when Days >= 2
              * [7] when Days >= 0
              */
+            [JsonProperty("days_since_last_exposure_level_values")]
             public int[] DaysSinceLastExposureLevelValues { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
             /*
@@ -151,15 +176,19 @@ namespace Chino
              * [6] when Duration <= 30
              * [7] when Duration > 30
              */
+            [JsonProperty("duration_level_values")]
             public int[] DurationLevelValues { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
             /*
              * [0]-[7] App Defined
              */
+            [JsonProperty("transmission_risk_level_values")]
             public int[] TransmissionRiskLevelValues { get; set; } = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
+            [JsonIgnore]
             public IDictionary<object, object> Metadata { get; set; } = new Dictionary<object, object>();
 
+            [JsonProperty("minimum_risk_score")]
             public byte MinimumRiskScore { get; set; } = 0;
 
             // This weight parameter is not used.
@@ -178,6 +207,7 @@ namespace Chino
 
             #region These properties are available in iOS 12.5, and in iOS 13.6 and later.
 
+            [JsonProperty("minimum_risk_score_full_range")]
             public double MinimumRiskScoreFullRange { get; set; } = 0.0;
 
             #endregion
@@ -185,6 +215,7 @@ namespace Chino
         }
 
         // https://developer.apple.com/documentation/exposurenotification/enexposureconfiguration
+        [JsonObject]
         public class AppleExposureV2Configuration
         {
             // Configuring Duration
@@ -196,11 +227,19 @@ namespace Chino
              * [1] The near attenuation threshold.
              * [2] The medium attenuation threshold.
              */
+            [JsonProperty("attenuation_duration_thresholds")]
             public int[] AttenuationDurationThresholds { get; set; } = { 15, 33, 73 };
 
+            [JsonProperty("immediate_duration_weight")]
             public double ImmediateDurationWeight { get; set; } = 100;
+
+            [JsonProperty("near_duration_weight")]
             public double NearDurationWeight { get; set; } = 100;
+
+            [JsonProperty("medium_duration_weight")]
             public double MediumDurationWeight { get; set; } = 100;
+
+            [JsonProperty("other_duration_weight")]
             public double OtherDurationWeight { get; set; } = 100;
 
             public int DaysSinceLastExposureThreshold { get; set; } = 0;
@@ -208,6 +247,7 @@ namespace Chino
             // Configuring Infectiousness
 
             // Must Specify v2
+            [JsonProperty("infectiousness_for_days_since_onset_of_symptoms")]
             public IDictionary<long, Infectiousness> InfectiousnessForDaysSinceOnsetOfSymptoms { get; set; } = new Dictionary<long, Infectiousness>() {
                 { -14, Infectiousness.Standard },
                 { -13, Infectiousness.Standard },
@@ -240,17 +280,30 @@ namespace Chino
                 { 14, Infectiousness.Standard },
             };
 
+            [JsonProperty("infectiousness_when_days_since_onset_missing")]
             public Infectiousness InfectiousnessWhenDaysSinceOnsetMissing = Infectiousness.Standard;
 
+            [JsonProperty("infectiousness_high_weight")]
             public double InfectiousnessHighWeight { get; set; } = 100.0; // The range of this value is 0-250%
+
+            [JsonProperty("infectiousness_standard_weight")]
             public double InfectiousnessStandardWeight { get; set; } = 100.0; // The range of this value is 0-250%
 
             // Configuring Report Types
 
+            [JsonProperty("report_type_confirmed_clinical_diagnosis_weight")]
             public double ReportTypeConfirmedClinicalDiagnosisWeight { get; set; } = 100.0;
+
+            [JsonProperty("report_type_confirmed_test_weight")]
             public double ReportTypeConfirmedTestWeight { get; set; } = 100.0;
+
+            [JsonProperty("report_type_recursive_weight")]
             public double ReportTypeRecursiveWeight { get; set; } = 100.0;
+
+            [JsonProperty("report_type_self_reported_weight")]
             public double ReportTypeSelfReportedWeight { get; set; } = 100.0;
+
+            [JsonProperty("report_type_none_map")]
             public ReportType ReportTypeNoneMap { get; set; } = ReportType.ConfirmedTest;
 
             #endregion
