8938163ee44a20aadce9de77b3b4122b1acf3c75
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-07-20 16:41:49 +0900

    WIP: Sync server (Android)

---
+++ Sample.Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Sample.Common
+{
+    public static class Constants
+    {
+        public const string API_ENDPOINT = "https://en.keiji.dev/diagnosis_keys";
+
+        public const string CLUSTER_ID = "212458"; // 6 digits
+    }
+}
---
+++ Sample.Common/IEnServer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Chino;
+using Newtonsoft.Json;
+
+namespace Sample.Common
+{
+    public interface IEnServer
+    {
+        public Task UploadDiagnosisKeysAsync(
+            string clusterId,
+            IList<ITemporaryExposureKey> temporaryExposureKeyList,
+            ReportType defaultRportType = ReportType.ConfirmedClinicalDiagnosis,
+            RiskLevel defaultTrasmissionRisk = RiskLevel.Medium
+            );
+
+        public Task<IList<DiagnosisKeyEntry>> GetDiagnosisKeysListAsync(string clusterId);
+
+        public Task DownloadDiagnosisKeysAsync(DiagnosisKeyEntry diagnosisKeyEntry, string path);
+
+    }
+
+    [JsonObject]
+    public class DiagnosisKeyEntry
+    {
+        [JsonProperty("region")]
+        public int Region;
+
+        [JsonProperty("url")]
+        public string Url;
+
+        [JsonProperty("created")]
+        public long Created;
+    }
+}
