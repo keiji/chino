97155f2ada03b6166d47907187afd6179fc16be3
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-08-10 00:21:47 +0900

    Preauthorized API don't use JobScheduler

--- Chino.Android/ExposureNotificationClient.cs
+++ Chino.Android/ExposureNotificationClient.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using Android.App;
 using Android.Content;
-using Android.Runtime;
 
 using Nearby = Android.Gms.Nearby.NearbyClass;
 using Java.IO;
@@ -14,7 +13,6 @@ using AndroidTemporaryExposureKey = Android.Gms.Nearby.ExposureNotification.Temp
 
 using Logger = Chino.ChinoLogger;
 using Android.Gms.Common.Apis;
-using Android.App.Job;
 using System.Threading;
 
 [assembly: UsesFeature("android.hardware.bluetooth_le", Required = true)]
@@ -303,134 +301,81 @@ namespace Chino.Android.Google
             }
         }
 
-        [Service(Permission = PERMISSION_BIND_JOB_SERVICE)]
-        [Preserve]
-        class TemporaryExposureKeyReleasedJob : JobService
-        {
-            private const int JOB_ID = 0x04;
-
-            public static void Enqueue(Context context, JobSetting jobSetting)
-            {
-                JobInfo.Builder jobInfoBuilder = new JobInfo.Builder(
-                    JOB_ID,
-                    new ComponentName(context, Java.Lang.Class.FromType(typeof(TemporaryExposureKeyReleasedJob))))
-                    .SetOverrideDeadline(0);
-
-                if (jobSetting != null)
-                {
-                    jobSetting.Apply(jobInfoBuilder);
-                }
-
-                JobInfo jobInfo = jobInfoBuilder.Build();
-
-                JobScheduler jobScheduler = (JobScheduler)context.GetSystemService(JobSchedulerService);
-                int result = jobScheduler.Schedule(jobInfo);
-                if (result == JobScheduler.ResultSuccess)
-                {
-                    Logger.D("TemporaryExposureKeyReleasedJob scheduled");
-                }
-                else if (result == JobScheduler.ResultFailure)
-                {
-                    Logger.D("TemporaryExposureKeyReleasedJob schedule failed");
-                }
-            }
-
-            public override bool OnStartJob(JobParameters @params)
-            {
-                IExposureNotificationHandler? handler = null;
-                ExposureNotificationClient? enClient = null;
-                if (ApplicationContext is IExposureNotificationHandler exposureNotificationHandler)
-                {
-                    handler = exposureNotificationHandler;
-                    enClient = (ExposureNotificationClient)exposureNotificationHandler.GetEnClient();
-                }
-
-                if (enClient == null)
-                {
-                    Logger.E("TemporaryExposureKeyReleasedJob: enClient is null.");
-                    return false;
-                }
-                if (handler == null)
-                {
-                    Logger.E("TemporaryExposureKeyReleasedJob: handler is null.");
-                    return false;
-                }
-
-                _ = Task.Run(async () =>
-                {
-                    try
-                    {
-                        IList<TemporaryExposureKey> temporaryExposureKeys = await GetReleasedTemporaryExposureKeys(enClient);
-                        handler.TemporaryExposureKeyReleased(temporaryExposureKeys);
-                    }
-                    catch (ApiException exception)
-                    {
-                        if (exception.IsENException())
-                        {
-                            throw exception.ToENException();
-                        }
-                        throw exception;
-                    }
-                    finally
-                    {
-                        JobFinished(@params, false);
-                    }
-                });
-
-                return true;
-            }
-
-            public override bool OnStopJob(JobParameters @params)
-            {
-                Logger.E("TemporaryExposureKeyReleasedJob stopped.");
-                return false;
-            }
-
-            private async Task<IList<TemporaryExposureKey>> GetReleasedTemporaryExposureKeys(
-                ExposureNotificationClient enClient
-                )
-            {
-                TaskCompletionSource<Intent> taskCompletionSource = new TaskCompletionSource<Intent>();
-                BroadcastReceiver receiver = new PreAuthorizeReleasePhoneUnlockedBroadcastReceiver(taskCompletionSource);
-
-                CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(API_TIMEOUT_MILLIS);
-                using (cancellationTokenSource.Token.Register(() =>
-                 {
-                     Logger.D("cancellationTokenSource canceled.");
-                     taskCompletionSource.TrySetCanceled();
-                     ApplicationContext.UnregisterReceiver(receiver);
-                 }))
-                {
-                    ApplicationContext.RegisterReceiver(
-                        receiver,
-                        INTENT_FILTER_PRE_AUTHORIZE_RELEASE_PHONE_UNLOCKED
-                        );
-
-                    await enClient.EnClient.RequestPreAuthorizedTemporaryExposureKeyReleaseAsync();
-
-                    Intent intent = await taskCompletionSource.Task;
-
-                    IList<TemporaryExposureKey> temporaryExposureKeys = intent.GetParcelableArrayListExtra(EXTRA_TEMPORARY_EXPOSURE_KEY_LIST)
-                        .Cast<AndroidTemporaryExposureKey>()
-                        .Select(tek => (TemporaryExposureKey)new PlatformTemporaryExposureKey(tek))
-                        .ToList();
-
-                    return temporaryExposureKeys;
-                }
-            }
-        }
-
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public override async Task RequestPreAuthorizedTemporaryExposureKeyReleaseAsync()
         {
             Logger.D("RequestPreAuthorizedTemporaryExposureKeyReleaseAsync");
 
             CheckInitialized();
 
-            TemporaryExposureKeyReleasedJob.Enqueue(_appContext, TemporaryExposureKeyReleasedJobSetting);
-        }
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+            IExposureNotificationHandler? handler = null;
+            ExposureNotificationClient? enClient = null;
+            if (_appContext is IExposureNotificationHandler exposureNotificationHandler)
+            {
+                handler = exposureNotificationHandler;
+                enClient = (ExposureNotificationClient)exposureNotificationHandler.GetEnClient();
+            }
 
+            if (handler == null)
+            {
+                Logger.E("TemporaryExposureKeyReleasedJob: handler is null.");
+                return;
+            }
+            if (enClient == null)
+            {
+                Logger.E("TemporaryExposureKeyReleasedJob: enClient is null.");
+                return;
+            }
+
+            try
+            {
+                IList<TemporaryExposureKey> temporaryExposureKeys = await GetReleasedTemporaryExposureKeys(enClient);
+                handler.TemporaryExposureKeyReleased(temporaryExposureKeys);
+            }
+            catch (ApiException exception)
+            {
+                if (exception.IsENException())
+                {
+                    throw exception.ToENException();
+                }
+                throw exception;
+            }
+            finally
+            {
+            }
+        }
+
+        private async Task<IList<TemporaryExposureKey>> GetReleasedTemporaryExposureKeys(
+            ExposureNotificationClient enClient
+            )
+        {
+            TaskCompletionSource<Intent> taskCompletionSource = new TaskCompletionSource<Intent>();
+            BroadcastReceiver receiver = new PreAuthorizeReleasePhoneUnlockedBroadcastReceiver(taskCompletionSource);
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(API_TIMEOUT_MILLIS);
+            using (cancellationTokenSource.Token.Register(() =>
+            {
+                Logger.D("cancellationTokenSource canceled.");
+                taskCompletionSource.TrySetCanceled();
+                _appContext.UnregisterReceiver(receiver);
+            }))
+            {
+                _appContext.RegisterReceiver(
+                    receiver,
+                    INTENT_FILTER_PRE_AUTHORIZE_RELEASE_PHONE_UNLOCKED
+                    );
+
+                await enClient.EnClient.RequestPreAuthorizedTemporaryExposureKeyReleaseAsync();
+
+                Intent intent = await taskCompletionSource.Task;
+
+                IList<TemporaryExposureKey> temporaryExposureKeys = intent.GetParcelableArrayListExtra(EXTRA_TEMPORARY_EXPOSURE_KEY_LIST)
+                    .Cast<AndroidTemporaryExposureKey>()
+                    .Select(tek => (TemporaryExposureKey)new PlatformTemporaryExposureKey(tek))
+                    .ToList();
+
+                return temporaryExposureKeys;
+            }
+        }
     }
 
     class UnInitializedException : Exception
